c3272a62807c685100f2afcf47bdf4a844e5771f
danihadi <detal31392@gmail.com>
2024-04-19 20:28:11 +0700

    modify configuration

--- src/config/idev.php
+++ src/config/idev.php
@@ -3,8 +3,8 @@
 return [
     'theme' => 'default', // available : default, greeny, monochrome
     'app_name' => 'iDev EasyAdmin',
-    'app_logo' => null,
-    'app_favicon' => null,
+    'app_logo' => asset('easyadmin/idev/img/logo-idev.png'),
+    'app_favicon' => asset('easyadmin/idev/img/favicon.png'),
     'enable_role' => true,
     'copyright' => 'Copyright &copy; iDev Semarang',
 ];
