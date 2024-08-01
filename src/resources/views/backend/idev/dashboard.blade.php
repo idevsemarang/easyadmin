0a59328c943fd2dc21d3822a4bfb51c2421f3873
Dani Hadi <asusa43e2.rect@gmail.com>
2024-08-01 12:10:52 +0700

    Modify welcome message

--- src/resources/views/backend/idev/dashboard.blade.php
+++ src/resources/views/backend/idev/dashboard.blade.php
@@ -11,7 +11,7 @@
         <div class="row align-items-center">
           <div class="col-md-12">
             Hi, <b>{{ Auth::user()->name }} </b> 
-            {{ env('PROJECT_NAME', config('idev.app_name','iDev Admin') ) }}
+            {{ env('PROJECT_NAME', config('idev.app_name','iDev EasyAdmin') ) }}
 
             @if(config('idev.enable_role',true))
             You are logged in as <i>{{ Auth::user()->role->name }}</i> 
@@ -25,8 +25,13 @@
       <div class="col-12">
         <div class="card mb-4">
           <div class="card-body p-3">
-            <h3> Welcome to iDev Admin</h3>
-            <p>An easy way to make admin page with crud, import and export. Feel free to override available method</p>
+            <h3> Welcome to iDev EasyAdmin</h3>
+            <p>An easy way to make admin page with crud, import and export. <br>
+              Just run simple command below (without curly branches) to make your fantastic admin page
+              <pre>
+                php artisan idev:controller-maker --slug={your-route} --table={your-table}
+              </pre>
+              Feel free to override available method.</p>
           </div>
         </div>
       </div>
