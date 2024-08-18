b5a56281fe7746b9c3e464013bc0bfbf34d36152
Dani Hadi <asusa43e2.rect@gmail.com>
2024-08-18 21:56:14 +0700

    updating onlyview to accept select options

--- src/resources/views/backend/idev/fields/onlyview.blade.php
+++ src/resources/views/backend/idev/fields/onlyview.blade.php
@@ -3,5 +3,13 @@ $preffix_method = (isset($method))? $method."_": "";
 @endphp
 <div class="{{(isset($field['class']))?$field['class']:'form-group'}}">
     <label>{{(isset($field['label']))?$field['label']:'Label '.$key}}</label>
-    <div class="form-control"> <span id="{{$preffix_method}}{{(isset($field['name']))?$field['name']:'id_'.$key}}">{{(isset($field['value']))?$field['value']:''}}</span> </div>
+    @if(array_key_exists('options', $field))
+    @foreach($field['options'] as $key => $opt)
+        @if($opt['value'] == $field['value'])
+        <div class="form-control"> <span id="{{$preffix_method}}{{(isset($field['name']))?$field['name']:'id_'.$key}}">{{$opt['text']}}</span> </div>
+        @endif
+    @endforeach
+    @else
+        <div class="form-control"> <span id="{{$preffix_method}}{{(isset($field['name']))?$field['name']:'id_'.$key}}">{{(isset($field['value']))?$field['value']:''}}</span> </div>
+    @endif
 </div>
