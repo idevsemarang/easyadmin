765dcd983ebfbf7b84e604ab31eb351a8adeb835
idevsemarang <85169307+idevsemarang@users.noreply.github.com>
2025-05-12 22:35:59 +0700

    show hide password value

--- src/resources/views/backend/idev/fields/password.blade.php
+++ src/resources/views/backend/idev/fields/password.blade.php
@@ -1,7 +1,34 @@
-@php 
-$preffix_method = (isset($method))? $method."_": "";
+@php
+    $preffix_method = isset($method) ? $method . '_' : '';
+    $passFormId = $preffix_method . (isset($field['name']) ? $field['name'] : 'id_' . $key);
 @endphp
-<div class="{{(isset($field['class']))?$field['class']:'form-group'}}">
-    <label>{{(isset($field['label']))?$field['label']:'Label '.$key}}</label>
-    <input type="password" id="{{$preffix_method}}{{(isset($field['name']))?$field['name']:'id_'.$key}}" name="{{(isset($field['name']))?$field['name']:'name_'.$key}}" value="{{(isset($field['value']))?$field['value']:''}}" class="form-control idev-form" autocomplete="">
+
+<div class="{{ isset($field['class']) ? $field['class'] : 'form-group' }}">
+    <label>{{ isset($field['label']) ? $field['label'] : 'Label ' . $key }}</label>
+    <div class="input-group">
+        <input type="password" id="{{ $passFormId }}"
+            name="{{ isset($field['name']) ? $field['name'] : 'name_' . $key }}"
+            value="{{ isset($field['value']) ? $field['value'] : '' }}" class="form-control idev-form" autocomplete="off">
+        <button type="button" id="sh_{{$passFormId}}" class="btn btn-outline-secondary toggle-password" data-target="{{ $passFormId }}">
+            Show
+        </button>
+    </div>
 </div>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const btn = document.getElementById('sh_{{$passFormId}}');
+        btn.addEventListener('click', function() {
+            const input = document.getElementById(this.getAttribute('data-target'));
+            console.log(input);
+
+            if (input.type === 'password') {
+                input.type = 'text';
+                this.textContent = 'Hide';
+            } else {
+                input.type = 'password';
+                this.textContent = 'Show';
+            }
+        });
+    });
+</script>
